4d5706a3ae5802beec82be9eea5320dcee137b30
Andriy Levanidov <alevanidov@kcura.com>
2013-10-29 12:07:16 -0500

    tries counter

--- stopwatch.py
+++ stopwatch.py
@@ -4,6 +4,9 @@ import simplegui
 
 # define global variables
 tick  = 0
+tries = 0
+click = 0
+on = False
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -16,14 +19,26 @@ def format(t):
 
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
+    global tick, on, tries, click
+    on = True
     timer.start()
     
 def stop():
+    global tick, on, tries, click
     timer.stop()
+    if on:
+        if tick % 10 == 0:
+            tries += 1 
+        click += 1         
+        on = False
+    
     
 def reset():
-    global tick
-    tick = 0
+    global tick, on, tries, click
+    tick  = 0
+    tries = 0
+    click = 0
+    on = False
 
 # define event handler for timer with 0.1 sec interval
 def timer_handler():
@@ -33,6 +48,8 @@ def timer_handler():
 # define draw handler
 def draw_handler(canvas):
     canvas.draw_text(format(tick), [100,100], 50, "Red")
+    ratio = str(click)+"/"+str(tries)
+    canvas.draw_text(ratio, [250, 20], 20, "Red")
     
 # create frame
 frame = simplegui.create_frame("Stop Watch", 300, 300)
